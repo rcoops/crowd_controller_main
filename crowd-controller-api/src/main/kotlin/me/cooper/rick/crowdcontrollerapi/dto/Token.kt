0dd68e8379dcdc94faa14593f959fbdc9b56b893
rcoops <R.P.Cooper1@edu.salford.ac.uk>
2018-02-28 19:49:47 +0000

    more jackson

--- crowd-controller-api/src/main/kotlin/me/cooper/rick/crowdcontrollerapi/dto/Token.kt
+++ crowd-controller-api/src/main/kotlin/me/cooper/rick/crowdcontrollerapi/dto/Token.kt
@@ -2,9 +2,22 @@ package me.cooper.rick.crowdcontrollerapi.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class Token(@JsonProperty("access_token") val accessToken: String? = null,
-                 @JsonProperty("token_type") val tokenType: String = "bearer",
-                 @JsonProperty("expires_in") val expiresIn: Int = -1,
-                 val scope: String = "read",
-                 val jti: String? = null,
-                 val user: UserDto? = null)
+data class Token(
+        @param:JsonProperty("access_token")
+        @get:JsonProperty("access_token")
+        val accessToken: String? = null,
+
+        @param:JsonProperty("token_type")
+        @get:JsonProperty("token_type")
+        val tokenType: String = "bearer",
+
+        @param:JsonProperty("expires_in")
+        @get:JsonProperty("expires_in")
+        val expiresIn: Int = -1,
+
+        val scope: String = "read",
+
+        val jti: String? = null,
+
+        val user: UserDto? = null
+)
