3788367bef9ab2c4e04a120805be360a1faa3775
rcoops <R.P.Cooper1@edu.salford.ac.uk>
2018-03-15 15:56:23 +0000

    convenience method for turning friends to group members

--- crowd-controller-api/src/main/kotlin/me/cooper/rick/crowdcontrollerapi/dto/group/GroupMemberDto.kt
+++ crowd-controller-api/src/main/kotlin/me/cooper/rick/crowdcontrollerapi/dto/group/GroupMemberDto.kt
@@ -1,5 +1,13 @@
 package me.cooper.rick.crowdcontrollerapi.dto.group
 
+import me.cooper.rick.crowdcontrollerapi.dto.user.FriendDto
+
 data class GroupMemberDto(val id: Long = 0,
                           val username: String = "",
-                          val groupAccepted: Boolean = false)
+                          val groupAccepted: Boolean = false) {
+
+    companion object {
+        fun fromFriendDto(dto: FriendDto) = GroupMemberDto(dto.id, dto.username)
+    }
+
+}
