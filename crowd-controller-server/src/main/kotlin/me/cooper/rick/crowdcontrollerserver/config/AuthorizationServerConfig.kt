01fa3004a5b1e4f56d0e011ead56c2096a803abf
rcoops <r.p.cooper1@edu.salford.ac.uk>
2018-04-12 13:13:51 +0100

    update pom to use correct api

--- crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/config/AuthorizationServerConfig.kt
+++ crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/config/AuthorizationServerConfig.kt
@@ -19,7 +19,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
 import java.util.*
 
-
 @Configuration
 @EnableAuthorizationServer
 internal class AuthorizationServerConfig(
