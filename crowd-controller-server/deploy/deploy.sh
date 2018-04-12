43d6f642b912b38e459ec1c173c9d768c5d2d80d
rcoops <r.p.cooper1@edu.salford.ac.uk>
2018-04-12 13:41:13 +0100

    updated deploy files

--- crowd-controller-server/deploy/deploy.sh
+++ crowd-controller-server/deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'SHUTTING DOWN APP'
-ssh rick@crowdcontroller.ddns.net <<< $'sudo systemctl stop CrowdController'
+ssh rick@crowdcontroller.ddns.net <<< $'sudo service crowdcontroller stop'
 echo 'SHUTDOWN END'
 echo 'REMOVING OLD JAR'
 ssh rick@crowdcontroller.ddns.net <<< $'rm /var/crowdcontroller/crowd-controller-server.jar'
@@ -10,5 +10,5 @@ echo 'DEPLOYING NEW JAR'
 sftp rick@crowdcontroller.ddns.net <<< $'put target/crowd-controller-server-*.jar /var/crowdcontroller/crowd-controller-server.jar'
 echo 'JAR DEPLOYMENT END'
 echo 'STARTING APP'
-ssh rick@crowdcontroller.ddns.net <<< $'sudo systemctl start CrowdController'
+ssh rick@crowdcontroller.ddns.net <<< $'sudo service crowdcontroller start'
 echo 'APP START END'
