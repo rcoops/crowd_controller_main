6df0001d4aad9ec87f3d9dde9aca2527604f2f15
rcoops <r.p.cooper1@edu.salford.ac.uk>
2018-04-13 21:52:39 +0100

    added logging against web socket

--- crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/controller/WebSocketController.kt
+++ crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/controller/WebSocketController.kt
@@ -2,6 +2,7 @@ package me.cooper.rick.crowdcontrollerserver.controller
 
 import me.cooper.rick.crowdcontrollerapi.dto.group.GroupDto
 import me.cooper.rick.crowdcontrollerapi.dto.user.UserDto
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestController
 class WebSocketController(private val simpMessagingTemplate: SimpMessagingTemplate) {
 
     fun send(groupDto: GroupDto) {
+        logger.debug("sending group dto: $groupDto")
         simpMessagingTemplate.convertAndSend("/topic/group/${groupDto.id}", groupDto)
     }
 
     fun send(vararg userDtos: UserDto) {
+        logger.debug("sending user dto(s): ${userDtos.map { it.toString() } }")
         userDtos.forEach { simpMessagingTemplate.convertAndSend("/topic/user/${it.id}", it) }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
     }
 
 }
