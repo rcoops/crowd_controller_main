3a090c4fc35c3d40e61d9ace882987c3b8d5842c
rcoops <R.P.Cooper1@edu.salford.ac.uk>
2018-05-05 20:12:53 +0100

    fix websocket update

--- crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/persistence/listeners/FriendshipListener.kt
+++ crowd-controller-server/src/main/kotlin/me/cooper/rick/crowdcontrollerserver/persistence/listeners/FriendshipListener.kt
@@ -3,6 +3,7 @@ package me.cooper.rick.crowdcontrollerserver.persistence.listeners
 import me.cooper.rick.crowdcontrollerserver.AutowireHelper
 import me.cooper.rick.crowdcontrollerserver.controller.WebSocketController
 import me.cooper.rick.crowdcontrollerserver.persistence.model.Friendship
+import me.cooper.rick.crowdcontrollerserver.service.UserService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,11 +17,15 @@ class FriendshipListener {
     @Autowired
     private var controller: WebSocketController? = null
 
+    @Autowired
+    private var userService: UserService? = null
+
     @PostPersist
     @PostUpdate
     internal fun postFriendshipUpdate(friendship: Friendship) {
         AutowireHelper.autowire(this)
-        controller?.send(friendship.invitee!!.toDto(), friendship.inviter!!.toDto())
+        userService?.user(friendship.invitee!!.id)?.let { controller?.send(it) }
+        userService?.user(friendship.inviter!!.id)?.let { controller?.send(it) }
         LOG.debug(friendship.toString())
     }
 
